3ed3a9fb3a9afcb72e4540761080841755166a0e
michelegelmini <michelegelminivr@gmail.com>
2024-05-16 20:12:37 +0200

    add: snack1

---
+++ js-jsnacks/snack01/main.js
@@ -0,0 +1,15 @@
+const firstNumber = parseInt(Number(prompt('Inserisci il primo numero')));
+const secondNumber = parseInt(Number(prompt('Inserisci il secondo numero')));
+
+
+if (firstNumber > secondNumber) {
+    console.log(firstNumber)
+} else if (firstNumber < secondNumber) {
+    console.log(secondNumber)
+}
+else if (firstNumber === secondNumber) {
+    console.log('i numeri sono uguali')
+}
+else if (!(firstNumber == NaN || secondNumber == NaN)) {
+    console.log('Non hai inserito due numeri')
+}
